3bfd18c3e14a0f010c7aab48110c2222bd0e184e
Al kahaf Ahmad <alkahafahmad@Als-MacBook-Air.local>
2025-06-01 12:30:35 +0530

    Publishing changes in dev

--- quest_aws/quest_aws_stack.py
+++ quest_aws/quest_aws_stack.py
@@ -46,7 +46,7 @@ class QuestAwsStack(Stack):
         # Add stages
         pipeline.add_stage(DevStage(self, "DevStage"))
 
-        prod_stage = ProdStage(self, "ProdStage")
-        pipeline.add_stage(prod_stage,
-            pre=[pipelines.ManualApprovalStep("ApproveProdDeploy")]
-        )
+        # prod_stage = ProdStage(self, "ProdStage")
+        # pipeline.add_stage(prod_stage,
+        #     pre=[pipelines.ManualApprovalStep("ApproveProdDeploy")]
+        # )
